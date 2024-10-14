72e20d9cb7cdf5492764a3bd621d64a940716645
jydykun <97005214+jydykun@users.noreply.github.com>
2024-10-15 00:38:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     sqlite_uri = "sqlite:///theblog.db"
 
 
-    SQLALCHEMY_DATABASE_URI = mysql_uri
+    SQLALCHEMY_DATABASE_URI = sqlite_uri
     SECRET_KEY = "dev"
     RECAPTCHA_PUBLIC_KEY = "6LdubDMqAAAAADlYsfdKWGeMR2vHoeG6LDhI78V_"
     RECAPTCHA_PRIVATE_KEY = "6LdubDMqAAAAADy5zk9EPk7LtZ4Asv-2IzFwPruu"
