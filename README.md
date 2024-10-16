2534baea0578f7c645a5c76836a10fecbc29040e
jydykun <97005214+jydykun@users.noreply.github.com>
2024-10-17 00:05:32 +0800

    Fixing some responsive bugs and update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# theblog
+# Draftly
  
--- config.py
+++ config.py
@@ -3,9 +3,10 @@ from datetime import timedelta
 class Config:
     mysql_uri = "mysql+pymysql://root:admin@127.0.0.1:3306/theblogdb"
     sqlite_uri = "sqlite:///theblog.db"
+    pssql_uri = "postgresql://theblogdb_fb1w_user:Atr94UXPP1aflWvJwMckVg5twPGN0db9@dpg-cs7bgkaj1k6c73cmmctg-a.oregon-postgres.render.com/theblogdb_fb1w"
 
 
-    SQLALCHEMY_DATABASE_URI = sqlite_uri
+    SQLALCHEMY_DATABASE_URI = mysql_uri
     SECRET_KEY = "dev"
     RECAPTCHA_PUBLIC_KEY = "6LdubDMqAAAAADlYsfdKWGeMR2vHoeG6LDhI78V_"
     RECAPTCHA_PRIVATE_KEY = "6LdubDMqAAAAADy5zk9EPk7LtZ4Asv-2IzFwPruu"
